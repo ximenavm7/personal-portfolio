e10a4fbef0c483d45600bf5a83636c95c2b8a69e
jimmyMsh <jimmymishan2004@gmail.com>
2024-07-16 21:52:45 -0400

    Add unit tests and configure test database - Updated `app.py` to support test mode via `TESTING` env variable.

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,17 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
-# Connection to the database through environment variables .env
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), 
-                     user=os.getenv("MYSQL_USER"), 
-                     password=os.getenv("MYSQL_PASSWORD"), 
-                     host=os.getenv("MYSQL_HOST"), 
-                     port=3306
-                     )
+if os.getenv("TESTING") == "true":
+    print("App running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    # Connection to the database through environment variables .env
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), 
+                        user=os.getenv("MYSQL_USER"), 
+                        password=os.getenv("MYSQL_PASSWORD"), 
+                        host=os.getenv("MYSQL_HOST"), 
+                        port=3306
+                        )
 
 #print(mydb)
 
