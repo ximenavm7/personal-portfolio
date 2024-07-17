2401979f076c621467569b93db263ff2a13891ff
jimmyMsh <jimmymishan2004@gmail.com>
2024-07-16 22:11:30 -0400

    Add edge case tests for timeline post validation

--- tests/test_app.py
+++ tests/test_app.py
@@ -79,4 +79,33 @@ class AppTestCase(unittest.TestCase):
         assert "<title>Timeline Posts</title>" in html
         assert '<form id="timeline-form"' in html 
         
+    # Tests for edge cases
+    def test_malformed_timeline_post(self):
+        # POST request with a missing name
+        response = self.client.post("/api/timeline_post", data={
+            "email" : "john@example.com",
+            "content" : "Hello world, I'm John!"
+        })
+        assert response.status_code == 400
+        html = response.get_data(as_text= True)
+        assert "Invalid name" in html
         
+        # POST request with empty content
+        response = self.client.post("/api/timeline_post", data={
+            "name": "John Doe",
+            "email": "john@example.com",
+            "content" : ""
+        })
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+        
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data ={
+            "name" : "John Doe",
+            "email" : "not-an-email",
+            "content" : "Hello world, I'm John!"
+        })
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "invalid email" in html
