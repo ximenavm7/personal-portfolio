21411c6e998e3743f85ec4589be6042526f95805
Ximena VM <122324007+ximenavm7@users.noreply.github.com>
2024-07-26 20:12:34 -0600

    Update redeploy-site.sh script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -6,32 +6,16 @@ cd /root/personal-portfolio
 # update git repo
 git fetch && git reset origin/main --hard
 
-#install new dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-deactivate
+# Spin containers down
+docker compose -f docker-compose.prod.yml down
 
-# Obtain credentials from user
-echo "MYSQL_USER: "
-read user
-echo "MYSQL_PASSWORD: "
-read password
-echo "MYSQL_DATABASE: "
-read db
-# Add extra information needed for Docker containers
-echo "MY_ROOT_PASSWORD: "
-read root
+# Build and run containers
+docker compose -f docker-compose.prod.yml up -d --build
 
-# Create new ENV file
-touch .env
-echo "URL=localhost:5000
-MYSQL_HOST=localhost
-MYSQL_USER=$user
-MYSQL_PASSWORD=$password
-MYSQL_DATABASE=$db
-MY_ROOT_PASSWORD=$root
-MARIADB_ROOT_PASSWORD=$root" > .env
-
-# Restart myportfolio service
-systemctl daemon-reload
-systemctl restart myportfolio
+# Check status of containers
+status=$(docker ps -q)
+if [ -n "$status" ]; then
+  echo "Deployment Successful :)"
+else
+  echo "Containers are not running :("
+fi
