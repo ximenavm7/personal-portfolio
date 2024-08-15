abed30bf84ac45ba1b8a58b4fec033e07e4142ac
ximenavm7 <ximena.vazquezmelladofs@udlap.mx>
2024-08-14 18:04:49 -0600

    Fixing issues tha test file presented

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change to the parent directory (project root)
-cd "$(dirname "$0")/.."
+cd /root/personal-portfolio
 
 # Activate the virtual environment
-source python3-virtualenv/bin/activate
+#source python3-virtualenv/bin/activate
 
 # Run the tests
-python -m unittest discover -v tests/
+python3-virtualenv/bin/python -m unittest discover -v tests/
